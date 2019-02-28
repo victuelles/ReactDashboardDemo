5da4a40562c7a1291612c96a40eff137eeeb29e4
Rom Vic <technoyguy@gmail.com>
2019-02-28 11:32:55 -0800

    key update

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://admin:password1@ds115595.mlab.com:15595/devconnector_db",
-  secretOrKey: "secret"
-};
-// mongoURI: "mongodb://admin:password1@ds143767.mlab.com:43767/interplay_db",
-//mongodb://admin:password1@ds143767.mlab.com:43767/interplay_db
-//mongoURI: "mongodb://admin:password1@ds115595.mlab.com:15595/devconnector_db",
-//Mongo Atlas
-//mongodb://admin:<PASSWORD>@cluster0-shard-00-00-pueih.mongodb.net:27017,cluster0-shard-00-01-pueih.mongodb.net:27017,cluster0-shard-00-02-pueih.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true
