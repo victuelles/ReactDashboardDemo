246a761cae851a04a284e2f35047cfe5e96f6c7f
rvictuelles <romvictuelles@Roms-MacBook-Pro-2.local>
2019-02-28 14:19:39 -0800

    redux dev tools conditional

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,20 @@
-import {createStore,applyMiddleware,compose} from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-const initialState={}
+const initialState = {};
 
-const middleWare=[thunk]
-
+const middleWare = [thunk];
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleWare),
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+      compose
+  )
+);
 
-export default store
+export default store;
