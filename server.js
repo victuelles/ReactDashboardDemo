fc80e380d4b4e1095d12ce057709a9bc0c553f40
Rom Vic <technoyguy@gmail.com>
2019-02-28 12:05:25 -0800

    new commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path=require('path')
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -35,6 +36,15 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 app.use("/api/essentials", essentials);
 app.use("/api/project", project);
+
+//serve static assets if in production
+if(process.env.NODE_ENV==='production'){
+  //set static folder
+  app.use(express.static('client/build'))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
